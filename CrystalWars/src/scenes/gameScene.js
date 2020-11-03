2cea6a6d5cd88c041d889fe4300f34c5b0de9acc
dario-bit <dario.uno@hotmail.com>
2020-11-03 13:35:00 +0100

    Invocar

--- CrystalWars/src/scenes/gameScene.js
+++ CrystalWars/src/scenes/gameScene.js
@@ -1,16 +1,15 @@
 nCards = 5;
+var cartas = [];
 
 class gameScene extends Phaser.Scene {
-    
+
     constructor() {
         super("gameScene")
     }
 
     create() {
         var board = this.add.image(0, 0, 'board');
-        Phaser.Display.Align.In.Center(board, this.add.zone(320,180,640,360));
-
-        var cartas = [];
+        Phaser.Display.Align.In.Center(board, this.add.zone(320, 180, 640, 360));
 
         for (let index = 0; index < nCards; index++) {
             cartas[index] = this.add.sprite(0, 0, 'card').setInteractive();
@@ -22,28 +21,42 @@ class gameScene extends Phaser.Scene {
 
             gameObject.x = dragX;
             gameObject.y = dragY;
-    
+
         });
 
         this.input.on('dragend', function (pointer, gameObject) {
 
             gameObject.clearTint();
-    
         });
 
         this.resizeCards(cartas);
     }
 
-    resizeCards(cartas){
+    resizeCards(cartas) {
         var ancho = 640;
         var anchoCarta = 56;
-        var anchoCartas = cartas.length*anchoCarta;
-        var cartaActual = (ancho/2)-(anchoCartas/2);
+        var anchoCartas = cartas.length * anchoCarta;
+        var cartaActual = (ancho / 2) - (anchoCartas / 2);
 
         for (let index = 0; index < cartas.length; index++) {
-            cartas[index].x = cartaActual+28;
+            cartas[index].x = cartaActual + 28;
             cartas[index].y = 319;
-            cartaActual+=anchoCarta;
-          }
+            cartaActual += anchoCarta;
+        }
+    }
+
+    update() {
+        //comprobar si la carta se ha usado
+        for (let index = 0; index < cartas.length; index++) {
+            if(cartas[index].y < 100){
+                this.invocar(index);
+            }
+        }
+    }
+
+    invocar(index) {
+        alert("alert");
+        cartas[index].setInteractive(false);
+        cartas.splice(index,1);
     }
 }
